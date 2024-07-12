ae5a3a5203f69471fffed37b05bc4f6137231014
DanteLopez08 <167150634+DanteLopez08@users.noreply.github.com>
2024-07-11 22:06:45 -0400

    Integrantes.py
    Integrantes

---
+++ Integrantes.py
@@ -0,0 +1,11 @@
+# Definir la lista de integrantes del grupo
+integrantes = [
+    "Dante Lopez",
+    "Luis Flores",
+    "Jordan Carrasco",
+    "Gerald Veas"
+]
+
+# Imprimir la lista de integrantes
+for integrante in integrantes:
+    print(integrante)
